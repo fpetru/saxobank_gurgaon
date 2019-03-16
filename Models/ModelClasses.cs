19f649c3de1928df500b5aaed01c81ea47aeddfb
maheshsabnis <sabnis_m@hotmail.com>
2019-03-16 12:07:45 +0530

    Add files via upload

---
+++ Models/ModelClasses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Core_webApp.Models
+{
+    public partial class Product
+    {
+        [Key] //Identity and Primary Key
+        public int ProductRowId { get; set; }
+        [Required(ErrorMessage ="ProdutId is Required")]
+        public string ProductId { get; set; }
+        [Required(ErrorMessage = "ProductName is Required")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "BasePrice is Required")]
+        public int BasePrice { get; set; }
+        [Required(ErrorMessage = "CategoryName is Required")]
+        public string CategoryName { get; set; }
+        [Required(ErrorMessage = "Manufacturer is Required")]
+        public string Manufacturer { get; set; }
+    }
+}
---
+++ Models/SaxoDbApp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_webApp.Models
+{
+    public class SaxoDbAppContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        /// <summary>
+        /// Read the ConenctionString from appsettings.json
+        /// when the class is registered in IServiceCollection in StartUp Class
+        ///from StartUp.cs
+        /// </summary>
+        /// <param name="options"></param>
+        public SaxoDbAppContext(DbContextOptions<SaxoDbAppContext> options):base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder); // Perform Default Operations for Object-to-table mapping
+        }
+    }
+}
